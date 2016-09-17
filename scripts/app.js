78e7a8d9b90e0d2821da6ab459ef094ffed0d2a4
captain-zhu <andypaker@qq.com>
2016-09-17 22:21:36 +0800

    finished first time

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,60 @@
 (function () {
   'use strict';
 
-  angular.module('CheckOffApp', [])
-    .controller('CheckOffController', CounterController);
+  angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  ToBuyShoppingController = function (ShoppingListCheckOffService) {
+    var vm = this;
+
+    vm.items = ShoppingListCheckOffService.getToBuyItems();
+
+    vm.buy = ShoppingListCheckOffService.buy();
+  };
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtShoppingController = function (ShoppingListCheckOffService) {
+    var vm = this;
+
+    vm.items = ShoppingListCheckOffService.getAlreadyBuyItems();
+  };
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    service.toBuyArray = [{
+      name: "cookies",
+      quantity: 10
+    },{
+      name: "cookies",
+      quantity: 9
+    },{
+      name: "cookies",
+      quantity: 8
+    },{
+      name: "cookies",
+      quantity: 7
+    },{
+      name: "cookies",
+      quantity: 6
+    }];
+    service.alreadyBuyArray = [];
+
+    service.getToBuyItems = function () {
+      return service.toBuyArray;
+    };
+
+    service.getAlreadyBuyItems = function () {
+      return service.alreadyBuyArray;
+    };
+
+    service.buy = function (index) {
+      var item = service.toBuyArray.splice(index, 1);
+      console.log("buy item: " + item);
+      service.alreadyBuyArray.push(item);
+    }
+  }
 })();
